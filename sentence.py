e1cdd7478af64771bc93c39bf6b09eec498e8b03
Shafigh <bit_!088>
2019-01-09 06:14:33 -0500

    update

---
+++ sentence.py
@@ -0,0 +1,8 @@
+class Sentence():
+    def __init__(self):
+        self.english = ""
+        self.french = ""
+        self.strength = 0
+        self.level = 1
+        self.bookmarked = False
+    # def __init__(self, english, ...) # need to define a new one? can have multiple ctors?
